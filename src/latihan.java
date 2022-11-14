386faeb3c1fcbc22658fb394b59561ae5ded6728
Sandi-PC <sandiyusuf01052004@gmail.com>
2022-11-14 14:44:46 +0700

    Sandi

---
+++ src/latihan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class latihan {
+    public static void main(String[] args) {
+
+        Scanner satu = new Scanner(System.in);
+        System.out.print("Masukan Nilai A : ");
+        double a = satu.nextDouble();
+
+        System.out.print("Masukan Nilai B : ");
+        double b = satu.nextDouble();
+
+        System.out.println(a+b);
+    }
+}
